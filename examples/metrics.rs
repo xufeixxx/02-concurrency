099cf65357a15a45a3cbb45290de3b9c91324c0a
xufei <xufei@xufei-VMware-Ubuntu24.04LTS>
2024-05-24 22:02:08 +0800

    feature: support dashmap

--- examples/metrics.rs
+++ examples/metrics.rs
@@ -8,7 +8,7 @@ const M: usize = 4;
 
 fn main() -> anyhow::Result<()> {
     let metrics = Metrics::new();
-    println!("{:?}", metrics.snapshot());
+    println!("{}", metrics);
 
     for idx in 0..N {
         task_worker(idx, metrics.clone())?;
@@ -22,8 +22,6 @@ fn main() -> anyhow::Result<()> {
         thread::sleep(Duration::from_secs(2));
         println!("{}", metrics);
     }
-
-    // anyhow::Ok(())
 }
 
 fn task_worker(idx: usize, metrics: Metrics) -> anyhow::Result<()> {
@@ -43,7 +41,7 @@ fn request_worker(metrics: Metrics) -> anyhow::Result<()> {
         loop {
             let mut rng = rand::thread_rng();
             thread::sleep(Duration::from_millis(rng.gen_range(50..800)));
-            let page = rng.gen_range(1..256);
+            let page = rng.gen_range(1..5);
             metrics.inc(format!("req.page.{}", page))?;
         }
         // anyhow::Ok(())
--- src/metrics.rs
+++ src/metrics.rs
@@ -2,42 +2,25 @@
 // 基本功能：inc/dec/snapshot
 
 use core::fmt;
-use std::{
-    collections::HashMap,
-    sync::{Arc, RwLock},
-};
-
-use anyhow::anyhow;
+use dashmap::DashMap;
+use std::sync::Arc;
 
 #[derive(Debug, Clone)]
 pub struct Metrics {
-    data: Arc<RwLock<HashMap<String, i64>>>,
+    data: Arc<DashMap<String, i64>>,
 }
 
 impl Metrics {
     pub fn new() -> Self {
         Metrics {
-            data: Arc::new(RwLock::new(HashMap::new())),
+            data: Arc::new(DashMap::new()),
         }
     }
 
     pub fn inc(&self, key: impl Into<String>) -> anyhow::Result<()> {
-        let mut data = self.data.write().map_err(|e| anyhow!(e.to_string()))?;
-        let counter = data.entry(key.into()).or_insert(0);
+        let mut counter = self.data.entry(key.into()).or_insert(0);
         *counter += 1;
         Ok(())
-    }
-
-    pub fn dec(&self, key: impl Into<String>) -> anyhow::Result<()> {
-        let mut data = self.data.write().map_err(|e| anyhow!(e.to_string()))?;
-        let counter = data.entry(key.into()).or_insert(0);
-        *counter -= 1;
-        Ok(())
-    }
-
-    pub fn snapshot(&self) -> anyhow::Result<HashMap<String, i64>> {
-        let data = self.data.read().map_err(|e| anyhow!(e.to_string()))?; // mutexguard impl deref
-        Ok(data.clone())
     }
 }
 
@@ -49,9 +32,8 @@ impl Default for Metrics {
 
 impl fmt::Display for Metrics {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let data = self.data.read().map_err(|_| fmt::Error {})?;
-        for (key, value) in data.iter() {
-            writeln!(f, "{}: {}", key, value)?;
+        for entry in self.data.iter() {
+            writeln!(f, "{}: {}", entry.key(), entry.value())?;
         }
         Ok(())
     }
